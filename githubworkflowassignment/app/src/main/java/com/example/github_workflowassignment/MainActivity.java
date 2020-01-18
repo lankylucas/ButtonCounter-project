43fdb37f12db4bc62178b03f1e98838586a31bbf
Anastasiya Romanova <romanovaaa@g.cofc.edu>
2020-01-18 16:56:09 -0500

    first commit

---
+++ githubworkflowassignment/app/src/main/java/com/example/github_workflowassignment/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.github_workflowassignment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+public static class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+}
