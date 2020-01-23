508e014f8a18a4ebbfadae79f9cd216100a7fa47
Anastasiya Romanova <romanovaaa@g.cofc.edu>
2020-01-23 15:57:43 -0500

    Commiting fist hope this works

---
+++ MyFirstAplicationRomanova/app/src/main/java/com/example/myfirstaplicationromanova/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myfirstaplicationromanova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.firstProj);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View v) {
+                Button btn = (Button) findViewById(R.id.firstProj);
+                btn.setText("FirstApp");
+            }
+        });
+    }
+}
+
+
+
